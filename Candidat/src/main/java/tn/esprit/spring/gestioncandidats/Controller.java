47f306f0b0136e4330c0f0908bc1f3eb06c21ca3
Bechir Ferjani <bechir_ferjani@yahoo.fr>
2026-04-23 21:29:03 +0100

    Add configuration files and initial setup for microservices

---
+++ Candidat/src/main/java/tn/esprit/spring/gestioncandidats/Controller.java
@@ -0,0 +1,20 @@
+package tn.esprit.spring.gestioncandidats;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidats")
+public class Controller {
+
+    @Value("${welcome.message}")
+    private String message;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return message;
+    }
+}
---
+++ Job/src/main/java/tn/esprit/spring/job/Controller.java
@@ -0,0 +1,18 @@
+package tn.esprit.spring.job;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Value("${welcome.message}")
+    private String message;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return message;
+    }
+}
